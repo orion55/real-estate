af3afc68d0bc51db1a6234fbcda6d02f3af67ffc
orion55 <grol55@mail.ru>
2017-01-04 20:42:01 +0500

    slider2

--- src/js/slider.js
+++ src/js/slider.js
@@ -10,4 +10,15 @@ $(document).ready(function () {
             pager: false
         }
     );
+    $('#slider2').bxSlider(
+        {
+            preloadImages: true,
+            // mode: 'fade',
+            controls: false,
+            auto: true,
+            autoStart: true,
+            pause: 5000,
+            pager: true
+        }
+    );
 });
